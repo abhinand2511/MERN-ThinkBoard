a5a679c2efa4f074f4ea65efb68a0ec8c8642bde
abhinand2511 <abhinand25112001@gmail.com>
2025-07-30 00:58:52 -0700

    Done with MongoDB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import { connectDB } from "./src/config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 
@@ -18,6 +22,10 @@ app.delete("/aa/bb", (req, res) => {
     res.status(200).json({message: "deleted successfully"});
 });
 
+connectDB();
+
 app.listen(5000, () => {
     console.log("Server started")
 })
+
+//mongodb+srv://abhinand25112001:zRfCDDjYiyKfuofC@cluster0.pfek0wt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
