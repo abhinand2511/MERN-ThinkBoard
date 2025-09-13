c1b0b1bf56bedd30478b1663195c174ff64f582e
abhinand2511 <abhinand25112001@gmail.com>
2025-09-13 02:10:14 -0700

    Navbar

--- frontend/Thinkboard/src/App.jsx
+++ frontend/Thinkboard/src/App.jsx
@@ -1,19 +1,10 @@
-import { Route, Routes } from "react-router";
+import React from 'react'
+import Navbar from './components/Navbar'
 
-import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/CreatePage";
-import NoteDetailPage from "./pages/NoteDetailPage";
+const HomePage = () => {
+  return <div className='min-h-screen'>
+    <Navbar/>
+  </div>
+}
 
-const App = () => {
-  return (
-    <div className="relative h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/note/:id" element={<NoteDetailPage />} />
-      </Routes>
-    </div>
-  );
-};
-export default App;
+export default HomePage
