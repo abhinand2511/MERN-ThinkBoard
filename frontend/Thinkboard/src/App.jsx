9218958911a537d3d468210d1b98e09d3b8ed30d
abhinand2511 <abhinand25112001@gmail.com>
2025-09-17 00:58:10 -0700

    few updates in app.jsx

--- frontend/Thinkboard/src/App.jsx
+++ frontend/Thinkboard/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
 
 const HomePage = () => {
   return <div className='min-h-screen'>
