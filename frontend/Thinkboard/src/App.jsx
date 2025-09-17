8b7f254a7464713d0772615fa822d9b65b528a74
abhinand2511 <abhinand25112001@gmail.com>
2025-09-17 00:39:35 -0700

    few updates in app.jsx

--- frontend/Thinkboard/src/App.jsx
+++ frontend/Thinkboard/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
 
 const HomePage = () => {
   return <div className='min-h-screen'>
-    <Navbar/>
+    <Routes>
+      <Route>path = "/" element = {<HomePage />}</Route>
+      <Route>path = "/create" element = {<CreatePage />}</Route>
+      <Route>path = "/note/:id" element = {<NoteDetailPage />}</Route>
+    </Routes>
   </div>
 }
 
